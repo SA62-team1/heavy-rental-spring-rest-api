3d5e973e93e19f1f63382659756bf9f6cea04f12
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-08-02 11:07:40 +0000

    HR-50 added Service classes to parse basic credentials and generate bearer token

---
+++ src/main/java/com/heavy_rental/rest_api/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.heavy_rental.rest_api.service;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.heavy_rental.rest_api.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)
+				.map(user -> org.springframework.security.core.userdetails.User.builder()
+						.username(user.getUsername())
+						.password(user.getPassword())
+						.disabled(!user.isEnabled())
+						.authorities(new SimpleGrantedAuthority(user.getRole()))
+						.build())
+				.orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+	}
+}
